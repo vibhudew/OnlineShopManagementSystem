9345dcb4c2e0af9a364313e2d72d0f667fa62c5e
riyajkafar <riyajkafar@gmail.com>
2021-04-19 14:48:08 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+
 
 Route::get('/Expenses', function () {
     return view('Expense/view');
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
