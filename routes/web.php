4e2a3e74fb6cf337518af1bb9038e91fb721a3a6
kaveencharuka <kaveencharuka@gmail.com>
2021-04-21 11:43:29 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,6 @@ Route::get('/Products', function () {
     return view('Product/viewproduct');
 });
 
-<<<<<<< HEAD
 Route::get('/Products/addproduct', function () {
     return view('Product/addproduct');
 });
@@ -68,36 +67,13 @@ Route::get('/Manufacturing', function () {
 
 
 Auth::routes();
-=======
-Route::get('/addproducts', function () {
-    return view('Product/addproduct');
-});
-
-Route::get('/Manufacturing', function () {    
-    return view('Manufacturing/addRecipe');
-});
-
-Route:: get('/Manufacturing',function(){
-    //We only return Recipe1 when saveing data but this view should appear other times aswell
-    $data=App\Models\Recipe::all();
-    return view('Manufacturing/Recipe')->with('Recipe1',$data); 
-    Route:: post('/saveRecipe','RecipeController@store');
-});
-
-
-
->>>>>>> 24082ae441382a975cda1f18818894b1ad165ce8
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-<<<<<<< HEAD
 
 
 
 
 
 
-=======
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
->>>>>>> 24082ae441382a975cda1f18818894b1ad165ce8
