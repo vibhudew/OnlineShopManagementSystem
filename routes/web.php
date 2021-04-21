6e86e73bb7a225777d303e5f730c912d9226591c
Shashika <kahatapitiyagsj@gmail.com>
2021-04-21 13:58:49 +0530

    delete

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/', function () {
 Route::get('/Expenses', function () {
     return view('Expense/view');
 });
-<<<<<<< HEAD
  Route::get('/Sales1', function () {
      return view('Sales/viewsales');
   });
@@ -29,26 +28,6 @@ Route::get('/Expenses', function () {
       return view('Sales/addsales');
    });
 
-=======
-
-Route::get('/AddExpenses', function () {
-    return view('Expense/add');
-});
-
- Route::get('/Sales', function () {
-     return view('Sales/addsales');
- });
-
-Route::get('/Sales', function () {
-    return view('Sales/viewsales');
-});
-//  Route::get('/Sales', function () {
-//      return view('Sales/addsales');
-//  });
-//Route::get('/Sales', function () {
-//     return view('Sales/updatesales');
-// });
->>>>>>> eb9400e22570fc942b94d11d7c890c260ec5683f
 
 Route::get('/Products', function () {
     return view('Product/viewproduct');
@@ -72,7 +51,6 @@ Route:: post('/saveRecipe','RecipeController@store');
 Auth::routes();
 
 
-<<<<<<< HEAD
  //Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
  Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -86,9 +64,6 @@ Auth::routes();
 
  Route:: post('/savesales','AddsalesController@store');
  Route:: post('/savesales','AddsalespaymentsController@store');
-=======
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
->>>>>>> eb9400e22570fc942b94d11d7c890c260ec5683f
 
 
 
