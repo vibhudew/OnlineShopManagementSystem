112c5a55da1120e5e89a603d9f0159117be0543f
VibhushaSingappuli <singappuli.dewan67@gmail.com>
2021-05-15 01:57:56 +0530

    Add Create for Employees

---
+++ resources/views/hrm/employees/create.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.master')
+
+@section('title', 'HRM')
+@section('page-title', 'HRM')
+@section('card-title', 'Employee Management')
+
+@section('content')
+
+<div id="app">
+        <div id="navbar">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary"" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">HRM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/HRM" >Employee</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Attendance" >Attendance</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Payroll" >Payroll</a>
+        </li>
+    
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+<div class="row">
+ <div class="col-sm-8 offset-sm-2">
+    <h1>Add a Employee</h1>
+  <div>
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('HRM.store') }}">
+          @csrf
+          <div class="form-group" class="mb-3">    
+              <label for="fname">First Name:</label>
+              <input type="text" class="form-control" name="fname"/>
+          </div>
+
+          <div class="form-group" class="mb-3">
+              <label for="lname">Last Name:</label>
+              <input type="text" class="form-control" name="lname"/>
+          </div>
+
+          <div class="form-group">
+                <label for="address">Address:</label>
+                <input type="text" class="form-control" name="address"  />
+            </div>
+            <div class="form-group">
+                <label for="nic">NIC:</label>
+                <input type="text" class="form-control" name="nic"/>
+            </div>
+           
+            <div class="form-group">
+                <label for="mobile">Mobile No:</label>
+                <input type="tel" class="form-control" name="mobile"  />
+            </div>
+            
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email"  />
+            </div>
+
+
+          <br />
+          <button type="submit" class="btn btn-primary">Create</button>
+      </form>
+  </div>
+</div>
+</div>
+
+@endsection
