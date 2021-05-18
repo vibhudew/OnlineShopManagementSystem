97722f3649260c9baa202a2637d6051bffa82c33
Shashika503 <kahatapitiyagsj@gmail.com>
2021-05-18 22:10:49 +0530

    AddChanges

--- app/Http/Controllers/AddSalesController.php
+++ app/Http/Controllers/AddSalesController.php
@@ -11,8 +11,9 @@ class AddsalesController extends Controller
         $addsales=new addsales;
 
         $this->validate($request,[
-            'Invoiceid'=>'required|numeric|max:10|min:5',
+            'Invoiceid'=>'required|max:10|min:5',
             'customer'=>'required|max:10|min:5',
+            'totamount'=>'required|numeric',
             ]);
 
         $addsales->invoiceid=$request->Invoiceid;
@@ -25,7 +26,7 @@ class AddsalesController extends Controller
         $addsales->save();
         $addsales=addsales::all();//getting all data from addsales table to data variable to display
         //dd($request->all()); 
-        return view('sales/viewsales')->with('viewsales1', $addsales );  //return addsales view with data to display
+        return view('Sales/viewsales')->with('viewsales1', $addsales );  //return addsales view with data to display
     }
     public function deleteviewsales($id){
 
@@ -42,19 +43,21 @@ class AddsalesController extends Controller
 
     public function editviewsales(Request $request){
 
-        $id = $request->id;
+        //  dd($request);
+
+         $id = $request->id;
        
-        $addsales=addsales::find($id);
-        $addsales->invoiceid=$request->Invoiceid;
-        $addsales->customer=$request->customer;
-	    $addsales->totalamount=$request->totamount;
-        $addsales->paymentmethod=$request->paymentmethod;
-        $addsales->payterm=$request->payterm;
-	    $addsales->paymentstatus=$request->paymentstatus;
-        $addsales->sellstatus=$request->sellstatus;
-        $addsales->save();
-        $addsales=addsales::all();
-        return view('Sales/viewsales')->with('editsale', $addsales );
+         $addsales=addsales::find($id);
+         $addsales->invoiceid=$request->Invoiceid1;
+         $addsales->customer=$request->customer1;
+	     $addsales->totalamount=$request->totamount1;
+         $addsales->paymentmethod=$request->paymentmethod1;
+         $addsales->payterm=$request->payterm1;
+	     $addsales->paymentstatus=$request->paymentstatus1;
+         $addsales->sellstatus=$request->sellstatus1;
+         $addsales->save();
+         $addsales=addsales::all();
+         return view('Sales/viewsales')-> with('viewsales1', $addsales );
 
     }
 
