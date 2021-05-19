6c95c60025128842b884a55232aecd099215da17
VibhushaSingappuli <singappuli.dewan67@gmail.com>
2021-05-19 19:48:14 +0530

    vendor\composer

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '36e13429c2beed37834ece6908fa29a6bb4bf64e',
+    'reference' => 'fcd092940ceac62b320d0e4ee624d4dde0988903',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -469,7 +469,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '36e13429c2beed37834ece6908fa29a6bb4bf64e',
+      'reference' => 'fcd092940ceac62b320d0e4ee624d4dde0988903',
     ),
     'laravel/sail' => 
     array (
@@ -1662,7 +1662,6 @@ self::$canGetVendors = method_exists('Composer\Autoload\ClassLoader', 'getRegist
 $installed = array();
 
 if (self::$canGetVendors) {
-
 foreach (ClassLoader::getRegisteredLoaders() as $vendorDir => $loader) {
 if (isset(self::$installedByVendor[$vendorDir])) {
 $installed[] = self::$installedByVendor[$vendorDir];
