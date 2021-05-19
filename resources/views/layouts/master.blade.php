24ec6163582ce344bf21f738dbf4c38a0ec54f5b
nimedha9 <shashininimedha98@gmail.com>
2021-05-19 23:47:32 +0530

    add

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,19 +7,7 @@
           <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
             <title>Storemaster - @yield('title')</title>
-<<<<<<< HEAD
 
-          <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-          <!--     Fonts and icons     -->
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-          <!-- CSS Files -->
-          <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-          <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
-          <!-- CSS Just for demo purpose, don't include it in your project -->
-          <link href="../assets/demo/demo.css" rel="stylesheet" />
-=======
-          
             <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
             <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -36,7 +24,6 @@
             <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.bootstrap4.min.js"></script>
             <script src="/vendor/datatables/buttons.server-side.js"></script>
             <script src="http://127.0.0.1:8000/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
->>>>>>> 2d001a1197152e6a0b331d175b42ef8b0c31186d
         </head>
 
         <body class="">
@@ -250,11 +237,7 @@
                       </div>
                     </div>
                   </div>
-<<<<<<< HEAD
-
-=======
                 </div>
->>>>>>> 2d001a1197152e6a0b331d175b42ef8b0c31186d
                 </div>
               <footer class="footer">
                 <div class=" container-fluid ">
@@ -284,33 +267,5 @@
                   </div>
                 </div>
               </footer>
-<<<<<<< HEAD
-            </div>
-          </div>
-          <!--   Core JS Files   -->
-          <script src="../assets/js/core/jquery.min.js"></script>
-          <script src="../assets/js/core/popper.min.js"></script>
-          <script src="../assets/js/core/bootstrap.min.js"></script>
-          <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-          <!--  Google Maps Plugin    -->
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-          <!-- Chart JS -->
-          <script src="../assets/js/plugins/chartjs.min.js"></script>
-          <!--  Notifications Plugin    -->
-          <script src="../assets/js/plugins/bootstrap-notify.js"></script>
-          <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-          <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-          <script src="../assets/demo/demo.js"></script>
-        </body>
-
-        </html>
-
-
-
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-=======
->>>>>>> 2d001a1197152e6a0b331d175b42ef8b0c31186d
      </body>
 </html>
