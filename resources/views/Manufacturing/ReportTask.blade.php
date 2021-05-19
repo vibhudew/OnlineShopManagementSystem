25f62ccab5a6516a9266bc984a8bb1000c4facf3
virajA404 <abeywardana21@gmail.com>
2021-05-20 00:00:03 +0530

    color change

--- resources/views/Manufacturing/ReportTask.blade.php
+++ resources/views/Manufacturing/ReportTask.blade.php
@@ -5,25 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Report</title>
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> --}}
+    
     <style>
         #tb1{
             font-family: Arial, Helvetica, sans-serif;
              border-collapse: collapse;
             width: 100%; 
+           
         }
         #tb1 td,#tb1 th{
             border: 1px solid #dddddd;
             padding: :8px;
         }
         #tb1 tr:nth-child(even){
-            background-color: #0bfdfd; 
+            background-color: #91ebd4; 
         }
         #tb1 th{
             padding-top:12px;
             padding-bottom: 12px;
             text-align: left;
-            background-color: aquamarine;
+            background-color: rgb(28, 224, 156);
             color:#fff;
 
         }
