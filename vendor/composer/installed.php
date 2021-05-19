d201b441507a137273dab264904d51e24dd73a29
Shashika503 <kahatapitiyagsj@gmail.com>
2021-05-19 23:28:34 +0530

    barryvdh package

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,11 +6,7 @@
     'aliases' => 
     array (
     ),
-<<<<<<< HEAD
-    'reference' => 'fcd092940ceac62b320d0e4ee624d4dde0988903',
-=======
-    'reference' => 'c4e31d177a4cf95c2acfce33dd50943a50a23ea9',
->>>>>>> 9dd1f2465b97260913ef75fd6f74776b8b9bbc34
+    'reference' => 'ea77e71028a1077da78185c7eed52a4898c47fd8',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -23,6 +19,15 @@
       array (
       ),
       'reference' => '9cb795bf30988e8c96dd3c40623c48a877bc6714',
+    ),
+    'barryvdh/laravel-dompdf' => 
+    array (
+      'pretty_version' => 'v0.9.0',
+      'version' => '0.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5b99e1f94157d74e450f4c97e8444fcaffa2144b',
     ),
     'brick/math' => 
     array (
@@ -73,6 +78,15 @@
       array (
       ),
       'reference' => 'e864bbf5904cb8f5bb334f99209b48018522f042',
+    ),
+    'dompdf/dompdf' => 
+    array (
+      'pretty_version' => 'v1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8768448244967a46d6e67b891d30878e0e15d25c',
     ),
     'dragonmantank/cron-expression' => 
     array (
@@ -449,11 +463,7 @@
       'aliases' => 
       array (
       ),
-<<<<<<< HEAD
-      'reference' => 'fcd092940ceac62b320d0e4ee624d4dde0988903',
-=======
-      'reference' => 'c4e31d177a4cf95c2acfce33dd50943a50a23ea9',
->>>>>>> 9dd1f2465b97260913ef75fd6f74776b8b9bbc34
+      'reference' => 'ea77e71028a1077da78185c7eed52a4898c47fd8',
     ),
     'laravel/sail' => 
     array (
@@ -663,6 +673,24 @@
       array (
       ),
       'reference' => 'bae7c545bef187884426f042434e561ab1ddb182',
+    ),
+    'phenx/php-font-lib' => 
+    array (
+      'pretty_version' => '0.5.2',
+      'version' => '0.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ca6ad461f032145fff5971b5985e5af9e7fa88d8',
+    ),
+    'phenx/php-svg-lib' => 
+    array (
+      'pretty_version' => 'v0.3.3',
+      'version' => '0.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa61b65e612ce1ae15f69b3d223cb14ecc60e32',
     ),
     'phpdocumentor/reflection-common' => 
     array (
@@ -913,6 +941,15 @@
       array (
         0 => '4.1.1',
       ),
+    ),
+    'sabberworm/php-css-parser' => 
+    array (
+      'pretty_version' => '8.3.1',
+      'version' => '8.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd217848e1396ef962fb1997cf3e2421acba7f796',
     ),
     'sebastian/cli-parser' => 
     array (
