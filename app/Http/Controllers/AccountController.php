2c362cb0f5cef858fe5d833f33acd1150c4ccf46
mmperera <madushimperera9927@gmail.com>
2021-05-29 22:09:05 +0530

    new

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Accounts;
+use PDF;
 
 class AccountController extends Controller
 {
     public function store(Request $request){
+
+        $this->validate($request,[
+            'name'=>'required|min:3|max:50',
+            'accountNumber' => 'required|min:8|max:10',
+            'accountType' => 'required',
+            'balance'=> 'required | max:50|min:3',
+            'note'=> 'required | max:50|min:3',
+        ]);
 
        // dd($request->all());
          $accounts = new Accounts;
@@ -21,42 +30,50 @@ class AccountController extends Controller
          $accounts->save();
 
         $data1=Accounts::all();
-        return view('Accounts/accountView')->with('Account1',$data1);
+        return redirect('/Accounts')->with('Account1',$data1)->with('success', 'Account details added successfully!');
 
     }
+
+//search
+
+    public function searchAccount()
+    {
+        $search_text = $_GET['query'];
+        $accounts = Accounts::where('name', 'LIKE', '%' .$search_text. '%')->get();
+
+        return view('Accounts/searchAccount' , compact('accounts'));
+    }
+
+
+    
 //update
     public function accountUpdate($id){
-        $accounts=Accounts::find($id); //contain all data of manufact in manufactRecipe object
-        return view('Accounts/accountUpdate')->with('updateAcc',$accounts);
+        $accounts=Accounts::find($id); //contain all data of accounts
+        return view('Accounts/accountUpdate')->with('accounts',$accounts);
 
     }
     public function accountUpdate2(Request $request){
-        $this->validate($request,[
-            'name'=>'required|min:3|max:50',
-            'accountNumber' => 'required|min:8|max:10',
-            'accountType' => 'required',
-            'balance'=> 'required | max:50|min:3',
-            'note'=> 'required | max:50|min:3',
-        ]);
+
         
-        $id=$request->id;
-        $account=$request->name;
-        $number=$request->accountNumber; 
-        $type=$request->accountType;
-        $balance=$request->balance;
-        $note=$request->note;
-        $accountUpdate2= Accounts::find($id);
 
-        $accountUpdate2->name=$account;
-        $accountUpdate2->account_number=$number;
-        $accountUpdate2->account_type=$type;
-        $accountUpdate2->balance=$balance;
-        $accountUpdate2->note=$note;
-        $accountUpdate2->save();
-
-        $data3 = Accounts::all();
-        return redirect('Accounts')->with('Account1',$accountUpdate2);
-
+        //dd($request);
+      $id = $request->id;
+       
+       $accounts=Accounts::find($id);
+       
+        $accounts->Name=$request->name;
+        $accounts->AccountNumber=$request->accountNumber;
+        $accounts->AccountType=$request->accountType;
+        $accounts->Balance=$request->balance;
+        $accounts->Note=$request->note;
+        $accounts->save();
+       
+        $accounts=Accounts::all();
+       
+        
+        return redirect('/Accounts')->with('Account1',$accounts)->with('success', 'Account details updated successfully!');
+       
+       
     }
     public function deleteAccount($id){
         $accounts  =Accounts::find($id);
@@ -67,6 +84,13 @@ class AccountController extends Controller
 
         
 
+    }
+
+    public function ReportAccount(){
+
+        $accounts = Accounts::all();
+        $pdf = PDF::loadView('Accounts/reportAccount',compact('accounts'));
+        return $pdf-> download('accountsDetails.pdf');
     }
 }
 
